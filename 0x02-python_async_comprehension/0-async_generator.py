722fccd0586ce8b23065afcf2c711e10a151d74d
Yousef Ayman <yousefayman20003@gmail.com>
2024-05-07 11:52:53 -0700

    Upload tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module containing  a coroutine called async_generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Generator function that returns 10 random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
