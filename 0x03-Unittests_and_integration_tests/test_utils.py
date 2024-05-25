4b278ea946f05e6a433b792f2d85c90d80a9acf1
Yousef Ayman <yousefayman20003@gmail.com>
2024-05-25 15:30:57 -0700

    change

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,40 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Module contains units test for utils module"""
-import unittest
-from unittest.mock import Mock, patch
-
-from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
-from typing import Mapping, Sequence, Union, Dict
+import unittest
+from utils import (access_nested_map, get_json, memoize)
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Unit test to test access_nested_map function"""
+    '''class for testing access_nestd_map function
+    '''
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(
-            self,
-            nested_map: Mapping,
-            path: Sequence,
-            expected: Union[Dict, int]
-            ) -> None:
-        """Test access_nested_map function"""
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test that the method returns what it is supposed to
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
     ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map: Mapping,
-            path: Sequence,
-            exception: Exception
-            ) -> None:
-        """Tests When access_nested_map raises an expection correctly"""
-        with self.assertRaises(exception):
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for the respective inputs """
+        with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
